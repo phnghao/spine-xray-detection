28bcfca5d85e46a97f462d15f5590c9ca27d397a
phnghao <phnghao2005@gmail.com>
2025-12-20 00:07:08 +0700

    change train.py

--- model/yolo/train.py
+++ model/yolo/train.py
@@ -2,14 +2,15 @@ from ultralytics import YOLO
 import torch
 import argparse
 
-def train_yolo(data_yaml):
+def train_yolo(data_yaml, epochs = 100, batch =4, imgsz = 640):
     device = 0 if torch.cuda.is_available() else 'cpu'
     model = YOLO('yolov8n.pt')
     model.train(
         data = data_yaml ,
-        epochs=100,
-        imgsz=640,
+        epochs=epochs,
+        imgsz=imgsz,
         device = device, # 0 : GPU, else cpu 
+        batch = batch
     )
     return model
 
@@ -17,10 +18,13 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--yaml-file', required = True, type = str)
+    parser.add_argument('--epochs', type = int, default=100)
+    parser.add_argument('--batch-size', type = int, default=4)
+    parser.add_argument('--imgsz', type = int, default=640)
 
-    arg = parser.parse_args()
+    args = parser.parse_args()
 
-    train_yolo(arg.yaml_file)
+    train_yolo(args.yaml_file, epochs=args.epochs, batch = args.batch_size, imgsz = args.imgsz)
 
 if __name__ == '__main__':
     main()
