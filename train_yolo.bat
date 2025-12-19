0f24e93de216b2576bbe909388a3a57fe45b5b57
phnghao <phnghao2005@gmail.com>
2025-12-20 00:02:30 +0700

    change argument

--- train_yolo.bat
+++ train_yolo.bat
@@ -1,8 +1,11 @@
 @echo off
 
 set YAML=./data/yolo/data.yaml
+set EPOCHS=100
+set IMGSZ=640
 
 python -m model.yolo.train ^
-    --yaml-file %YAML%
-
+    --yaml-file %YAML% ^
+    --epochs %EPOCHS% ^
+    --imgsz %IMGSZ%
 pause
