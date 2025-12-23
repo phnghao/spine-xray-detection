14873e86624264e88d2913895481c4b3d2eb2f82
phnghao <phnghao2005@gmail.com>
2025-12-23 22:46:54 +0700

    + convert_dicom.bat

--- convert_dicom.bat
+++ convert_dicom.bat
@@ -1,11 +1,7 @@
 @echo off
-
-REM ===== Paths =====
-set INPUT_DIR=./data/raw/dataset
-set OUTPUT_DIR=./data/images/dataset
-set CPUS=4
-set LOG_FILE=./convert_train_log.txt
-set DEBUG=--debug
+REM =========================================
+REM  DICOM to PNG preprocessing
+REM =========================================
 
 REM ===== Create venv if not exists =====
 if not exist venv (
@@ -16,18 +12,52 @@ if not exist venv (
 REM ===== Activate venv =====
 call venv\Scripts\activate
 
-
-REM ===== Install dependencies (1st time safe) =====
+REM ===== Install dependencies =====
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
-REM ===== Run conversion =====
-echo Converting DICOM images to PNG...
+
+REM =========================================
+REM ========== TRAIN DATA ===================
+REM =========================================
+
+set INPUT_DIR=./data/raw/train_images
+set OUTPUT_DIR=./data/images/train_images
+set CPUS=4
+set LOG_FILE=./convert_train_log.txt
+
+set DEBUG=--debug
+
+echo -----------------------------------------
+echo Converting TRAIN DICOM to PNG...
+echo -----------------------------------------
+
 python preprocessing/dicom2png.py ^
-  --input-dir %INPUT_DIR% ^
-  --output-dir %OUTPUT_DIR% ^
+  --input-dir "%INPUT_DIR%" ^
+  --output-dir "%OUTPUT_DIR%" ^
   --cpus %CPUS% ^
-  --log-file %LOG_FILE% ^
+  --log-file "%LOG_FILE%" ^
+  %DEBUG%
+
+
+REM =========================================
+REM ========== TEST DATA =====================
+REM =========================================
+
+set INPUT_DIR=./data/raw/test_images
+set OUTPUT_DIR=./data/images/test_images
+set CPUS=4
+set LOG_FILE=./convert_test_log.txt
+
+echo -----------------------------------------
+echo Converting TEST DICOM to PNG...
+echo -----------------------------------------
+
+python preprocessing/dicom2png.py ^
+  --input-dir "%INPUT_DIR%" ^
+  --output-dir "%OUTPUT_DIR%" ^
+  --cpus %CPUS% ^
+  --log-file "%LOG_FILE%" ^
   %DEBUG%
 
 pause
